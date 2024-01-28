cf65665dc46bc620cc9d257d7d43867401b6b1d6
changhui98 <net_0102@naver.com>
2024-01-28 16:09:24 +0900

    1.0 initialize

---
+++ src/main/java/com/hui/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.hui.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hui/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hui.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
