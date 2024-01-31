41bba674b777b4ce68c56e1fcad217a4b0d6ccb0
changhui98 <net_0102@naver.com>
2024-02-01 01:42:39 +0900

    기존 연습용 코드 삭제

--- src/main/java/com/hui/blog/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hui.blog;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
---
+++ src/main/java/com/hui/blog/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package com.hui.blog.repository;
+
+import com.hui.blog.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
