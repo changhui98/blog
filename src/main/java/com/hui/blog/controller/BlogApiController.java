ddb6ef7257e59ff4ddf5d8396c51f9818a5a90b8
changhui98 <net_0102@naver.com>
2024-02-03 23:06:28 +0900

    Article service 코드 생성

---
+++ src/main/java/com/hui/blog/controller/BlogApiController.java
@@ -0,0 +1,27 @@
+package com.hui.blog.controller;
+
+import com.hui.blog.domain.Article;
+import com.hui.blog.dto.AddArticleRequest;
+import com.hui.blog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    // HTTP 메서드가 POST일 때 전달받은 URL과 동일하면 메서드로 매핑
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+}
